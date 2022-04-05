d84787935358673cbeee9e993ca704d69e1cb300
Moti Elmakies <motiphone2003@gmail.com>
2022-04-05 17:55:54 +0300

    :bug: test

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,31 @@
 import type { NextPage } from 'next'
 import Layout from '../pages/shared/Layouts/Layout'
-
+import 'semantic-ui-css/semantic.min.css'
+import { Button, Icon } from 'semantic-ui-react'
+import { Input } from 'semantic-ui-react'
 const Home: NextPage = () => {
-  return <Layout>test</Layout>
+  return (
+    <Layout>
+      {' '}
+      <Input loading icon="user" primary placeholder="חפש..." size="mini" />
+      <Button animated>
+        <Button.Content visible>Next</Button.Content>
+        <Button.Content hidden>
+          <Icon name="arrow right" />
+        </Button.Content>
+      </Button>
+      <Button animated="vertical">
+        <Button.Content hidden>Shop</Button.Content>
+        <Button.Content visible>
+          <Icon name="shop" />
+        </Button.Content>
+      </Button>
+      <Button animated="fade" primary>
+        <Button.Content visible>Sign-up for a Pro account</Button.Content>
+        <Button.Content hidden>$12.99 a month</Button.Content>
+      </Button>
+    </Layout>
+  )
 }
 
 export default Home
